0ce04b6753849e0c29fbfe4c5af6aea71a13c9a5
Austin <elektricwebdesign@gmail.com>
2017-08-02 12:40:39 -0600

    removed some logs and add some edge case handling

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ connection.connect((err) => {
 });
 // KICK THINGS OF FROM HERE!
 // lets show our user our sweet bamazon store
-function Bamazon(){
+const Bam = function (){
   products.getAllProducts().then((results) => {
     const productData = results.map((product) => {
       const { item_id, product_name, department_name, price, stock_quantity } = product;
@@ -28,52 +28,49 @@ function Bamazon(){
     console.log(columns.yellow);
     return Promise.resolve(productData);
   }).then((allProductInfo) => {
-    return promptForUserChoice(allProductInfo);
+    return promptForCustomerChoice(allProductInfo);
 
   }).catch((err) => console.log('errrr', err))
-}
-Bamazon();
-function promptForUserChoice(allProductInfo) {
+};
+Bam();
+function promptForCustomerChoice(allProductInfo) {
   const askUserForProduct = {
     type: 'input',
     message: 'What item would you like to purchase? Please Enter ITEM_ID...',
     name: 'item_id',
   };
-
   prompt(askUserForProduct).then((product) => {
     product.item_id = parseInt(product.item_id, 10)
-    if (product.item_id === '' || isNaN(product.item_id) ) throw new Error("Please enter a ITEM_ID");
-    return promptForcustomerQtny(product, allProductInfo);
+    if (product.item_id === '' || isNaN(product.item_id) || product.item_id > 10 ) {
+      throw new Error("Please enter a correct ITEM_ID");
+    }
+    return promptForCustomerQtny(product, allProductInfo);
   }).catch((err) => {
-     console.log(' ERROR RECIEVING CUSTOMER INPUT', err.toString().red);
-     promptForUserChoice();
+     console.log(err.toString().red);
+     promptForCustomerChoice(allProductInfo);
     })
 }
-
-function promptForcustomerQtny(product, allProductInfo) {
+function promptForCustomerQtny(product, allProductInfo) {
     const askUserForQtny = {
         type: 'input',
         message: `How many items would you like to purchase?`,
         name: 'customerQtny',
     };
-
     prompt(askUserForQtny).then((qtny) => {
-      // console.log('OUR QTNY@!!!!',product, allProductInfo, qtny);
       qtny.customerQtny = parseInt(qtny.customerQtny, 10)
       return checkForAvailableQtny(product, qtny, allProductInfo);
     }).catch((err) => console.warn(err))
 }
-
 function checkForAvailableQtny({ item_id }, { customerQtny }, allProductInfo) {
   let avaiableProduct = allProductInfo.filter((item) => {
     if (item.item_id === item_id) {
       if(item.stock_quantity >= customerQtny) {
          item.stock_quantity = item.stock_quantity - customerQtny;
-        return completeCustomerTransaction(item, customerQtny,)
+        return completeCustomerTransaction(item, customerQtny);
       }
-      return insufficentQtny(item)
+      return insufficentQtny(item);
     }
-  })
+  });
 }
 function completeCustomerTransaction(item, customerQtny) {
   let totalSale = customerQtny * item.price;
@@ -81,25 +78,25 @@ function completeCustomerTransaction(item, customerQtny) {
     console.group();
     console.log(`THANK YOU FOR YOUR PURCHASE! YOUR TOTAL COMES TO $${totalSale}.00`.green);
     console.log(' PLEASE FEEL FREE TO BROWSE AROUND SOME MORE!'.green);
-    Bamazon();
+    Bam();
     console.endGroup();
-  }).catch((err) => new Error("Looks like there was a problem making your tranasaction", err))
+  }).catch((err) => new Error("Looks like there was a problem making your tranasaction", err));
 }
 function insufficentQtny(product) {
   if (product.product_name === 'fucks') {
     return (
       console.group(),
-      console.log(`Looks like there are no more ${product.product_name} to give today...\n`.red),
+      console.log(`Looks like there are no more ${product.product_name.toUpperCase()} to give today...\n`.red),
       console.groupEnd(),
       console.group(),
-      console.log(`Please try entering a lower Qtny.`.green),
+      console.log(`Please try entering a lower quantity.`.green),
       console.groupEnd(),
-      Bamazon()
-    )
+      Bam()
+    );
   }
-  console.group()
-  console.log(`Sorry Looks like ther is insuffecient qtny for ${product.product_name}`.red);
-  console.log(`Please try entering a lower Qtny.`.green),
+  console.group();
+  console.log(`Sorry Looks like ther is insuffecient quantity for ${product.product_name.toUpperCase()}\n`.red),
+  console.log(`Please try entering a lower quantity.`.green),
   console.groupEnd();
-  Bamazon();
+  Bam();
 }
